478414262e474961a6983eeb231dd0b4131966f6
Claus Jørgensen <clausbmj@gmail.com>
2020-04-20 13:08:50 +0200

    now takes an input as well

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+	String input = System.in.readline();
+
         System.out.println("Hello World of Git!");
+
+	System.out.println(input);
+
+        System.out.println("This adds another line!");
     }
 }
